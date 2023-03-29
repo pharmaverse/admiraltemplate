04a407a813f1c7338cb32fa749299f8453cdeb5b
Zelos Zhu <zdz2101@github.com>
2023-03-29 15:00:30 +0000

    add blurb in roxygen details tag

--- R/my_first_fcn.R
+++ R/my_first_fcn.R
@@ -6,7 +6,10 @@
 #'
 #' @author Open Source
 #'
-#' @details Something Something
+#' @details Below this in the roxygen documentation you will find tags for family and keywords.
+#' This is to create organized sections for the Reference tab on the pkgdown website.
+#' You can modify the `_pkgdown.yml` as necessary to create appropriate sections as necessary.
+#' Under `./man/roxygen/meta.R`, you will find where to store these family/keywords.
 #'
 #' @return Happy Message
 #'
